3aa8b8001f0c5fe86c986c1200f7a550617619d8
Maxim-Bernashevsky <0112358bern@mail.ru>
2018-05-31 15:26:29 +0300

    add webpack config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.styl';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+
+          <img className="App-logo" src={logo} width={190} height={190}/>
+
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractCSS = new ExtractTextPlugin('[name].css');
+
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  mode: 'production',
+  target: "web",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.svg$/,
+        // test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            query: { limit : 10000 }
+          }
+        ],
+      },
+      {
+        test: /\.styl$/,
+        use: extractCSS.extract([ 'css-loader', 'stylus-loader' ]) // , 'postcss-loader'
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'public/index.html',
+      favicon: 'public/favicon.ico',
+      title: 'AURUS Club',
+    }),
+    extractCSS,
+  ],
+};
