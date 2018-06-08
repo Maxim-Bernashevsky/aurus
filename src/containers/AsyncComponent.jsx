116237cdfc73549fc4c38a01c47388c88e1ee6b9
Maxim-Bernashevsky <0112358bern@mail.ru>
2018-06-08 18:08:33 +0300

    update asunc import components

---
+++ src/containers/AsyncComponent.jsx
@@ -0,0 +1,31 @@
+import React  from 'react';
+import Loadable from "react-loadable";
+
+const LoadingComponent = () => (<h1>Loading...</h1>);
+
+module.exports = {
+  Header: Loadable({
+    loader: () => import(/* webpackChunkName: "header" */ '../components/Header'),
+    loading: LoadingComponent,
+  }),
+  Footer: Loadable({
+    loader: () => import(/* webpackChunkName: "footer" */ '../components/Footer'),
+    loading: LoadingComponent,
+  }),
+  Home: Loadable({
+    loader: () => import(/* webpackChunkName: "home" */ './Home'),
+    loading: LoadingComponent,
+  }),
+  Senat: Loadable({
+    loader: () => import(/* webpackChunkName: "senat" */ './Senat'),
+    loading: LoadingComponent,
+  }),
+  Configurator: Loadable({
+    loader: () => import(/* webpackChunkName: "configurator" */ './Configurator'),
+    loading: LoadingComponent,
+  }),
+  Order: Loadable({
+    loader: () => import(/* webpackChunkName: "order" */ './Order'),
+    loading: LoadingComponent,
+  }),
+};
--- src/containers/Routers.jsx
+++ src/containers/Routers.jsx
@@ -7,40 +7,23 @@ import {
 } from 'react-router-dom';
 
 import Loadable from 'react-loadable';
-// import AsyncComponent from "../components/AsyncComponent";
+// import AsyncComponent from "./AsyncComponent";
 const LoadingComponent = () => (<h1>Loading...</h1>);
 
+import {Footer, Header, Home, Senat, Configurator, Order} from "./AsyncComponent"
 
-// let Header = AsyncComponent('Header');
-let Header = Loadable({
-  loader: () => import(/* webpackChunkName: "header" */ '../components/Header'),
-  loading: LoadingComponent,
-});
 
-let Footer = Loadable({
-  loader: () => import(/* webpackChunkName: "footer" */ '../components/Footer'),
-  loading: LoadingComponent,
-});
+// const Header = AsyncComponent('../components/Header');
+// const Footer = AsyncComponent('./../components/Footer');
 
-let Home = Loadable({
-  loader: () => import(/* webpackChunkName: "home" */ './Home'),
-  loading: LoadingComponent,
-});
+// const Home = AsyncComponent('./Home');
 
-let Senat = Loadable({
-  loader: () => import(/* webpackChunkName: "senat" */ './Senat'),
-  loading: LoadingComponent,
-});
+// const Senat = Loadable({
+//   loader: () => import(/* webpackChunkName: "senat" */ './Senat'),
+//   loading: LoadingComponent,
+// });
+//
 
-let Configurator = Loadable({
-  loader: () => import(/* webpackChunkName: "configurator" */ './Configurator'),
-  loading: LoadingComponent,
-});
-
-let Order = Loadable({
-  loader: () => import(/* webpackChunkName: "order" */ './Order'),
-  loading: LoadingComponent,
-});
 
 const NODE_ENV_PROD = process.env.NODE_ENV === 'production';
 const basename = NODE_ENV_PROD ? "/aurus" : '';
